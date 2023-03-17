a57b69378686329f02d26ce3360deffad683cd87
Kadir <kadirkopuk.ybs@gmail.com>
2023-03-17 12:08:45 +0300

    fixing-NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,32 @@
+import { Stack, Typography } from "@mui/material";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import React from "react";
 
 function NotFound() {
-  return <div>404 Not Found</div>;
+  return (
+    <Stack
+      sx={{
+        alignItems: "center",
+        opacity: 0.7,
+        pt: 6,
+        backgroundColor: "#f8f8f8",
+        height: "100vh",
+      }}
+    >
+      <Typography>
+        <SentimentVeryDissatisfiedIcon
+          sx={{
+            fontSize: 300,
+          }}
+        />
+      </Typography>
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h2">Page Not Found</Typography>
+      <Typography variant="p1">
+        The Page you are looking for doesn't exist or an other error occurred.
+      </Typography>
+    </Stack>
+  );
 }
 
 export default NotFound;
